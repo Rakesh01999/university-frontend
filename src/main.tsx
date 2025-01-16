0236043f5048314460b0d0a04217d6b99d38b75a
Rakesh Biswas <rbiswas01999@gmail.com>
2025-01-16 23:36:15 +0600

    Redux setup and a quick recap

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { RouterProvider } from 'react-router-dom'
-import router from './routes/routes.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/routes.tsx";
+import { Provider } from "react-redux";
+import { store } from "./redux/features/store.ts";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {/* <App /> */}
-    <RouterProvider router={router}/>
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>
+);
