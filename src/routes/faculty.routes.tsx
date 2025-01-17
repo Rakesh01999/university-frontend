554f084795ae017565b1d1ea6e131a29abc077ec
Rakesh Biswas <rbiswas01999@gmail.com>
2025-01-17 13:42:03 +0600

    student pages created

--- src/routes/faculty.routes.tsx
+++ src/routes/faculty.routes.tsx
@@ -1,14 +1,14 @@
-import FacultyDashboard from '../pages/faculty/FacultyDashboard';
-import OfferedCourse from '../pages/faculty/OfferedCourse';
+import FacultyDashboard from "../pages/faculty/FacultyDashboard";
+import OfferedCourse from "../pages/faculty/OfferedCourse";
 export const facultyPaths = [
   {
-    name: 'Dashboard',
-    path: 'dashboard',
+    name: "Dashboard",
+    path: "dashboard",
     element: <FacultyDashboard />,
   },
   {
-    name: 'Offered Course',
-    path: 'offered-course',
+    name: "Offered Course",
+    path: "offered-course",
     element: <OfferedCourse />,
   },
 ];
