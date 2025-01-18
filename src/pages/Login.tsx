44764210ea43c7ebbbfd77e519bba5f202114aaf
Rakesh Biswas <rbiswas01999@gmail.com>
2025-01-18 11:07:01 +0600

    set cookies in the browser

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,10 @@
-import { Button } from 'antd';
-import { useForm } from 'react-hook-form';
-import { useLoginMutation } from '../redux/features/auth/authApi';
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
 // import { useAppDispatch } from '../redux/hooks';
-import { setUser } from '../redux/features/auth/authSlice';
-import { verifyToken } from '../utils/verifyToken';
-import { useAppDispatch } from '../redux/features/hooks';
+import { setUser } from "../redux/features/auth/authSlice";
+import { verifyToken } from "../utils/verifyToken";
+import { useAppDispatch } from "../redux/features/hooks";
 
 const Login = () => {
   const dispatch = useAppDispatch();
@@ -12,19 +12,21 @@ const Login = () => {
     defaultValues: {
       // userId: 'A-0002',
       // password: 'admin123',
-      userId: 'A-0001',
-      password: 'ami12345',
+      userId: "A-0001",
+      password: "ami12345",
     },
   });
 
-  const [login, { error }] = useLoginMutation();
+  // const [login, { error }] = useLoginMutation();
+  const [login, { data, error }] = useLoginMutation();
+  console.log("data=>", data);
+  console.log("error=>", error);
 
   const onSubmit = async (data) => {
     const userInfo = {
       id: data.userId,
       password: data.password,
     };
- 
     const res = await login(userInfo).unwrap();
     const user = verifyToken(res.data.accessToken);
 
@@ -35,11 +37,11 @@ const Login = () => {
     <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label htmlFor="id">ID: </label>
-        <input type="text" id="id" {...register('userId')} />
+        <input type="text" id="id" {...register("userId")} />
       </div>
       <div>
         <label htmlFor="password">Password: </label>
-        <input type="text" id="password" {...register('password')} />
+        <input type="text" id="password" {...register("password")} />
       </div>
       <Button htmlType="submit">Login</Button>
     </form>
