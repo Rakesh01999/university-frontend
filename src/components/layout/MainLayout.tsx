650878ba159ee0906f4209f56b53a0e308f2380a
Rakesh Biswas <rbiswas01999@gmail.com>
2025-01-18 22:50:51 +0600

    MainLayout updated

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -6,18 +6,26 @@ import {
   VideoCameraOutlined,
 } from "@ant-design/icons";
 import { Children, createElement } from "react";
-import { NavLink, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { adminPaths, adminSidebarItems } from "../../routes/admin.routes";
 import { sidebarItemsGenerator } from "../../utils/sidebarItemsGenerator";
 import Sidebar from "./Sidebar";
-const { Header, Content, Footer, Sider } = Layout;
+import { useAppDispatch } from "../../redux/hooks";
+import { logout } from "../../redux/features/auth/authSlice";
+const { Header, Content } = Layout;
 
 const MainLayout = () => {
+  const dispatch = useAppDispatch();
+  const handleLogout = () => {
+    dispatch(logout());
+  };
   return (
     <Layout style={{ height: "100vh" }}>
       <Sidebar />
       <Layout>
-        <Header style={{ padding: 0 }} />
+        <Header>
+          <button onClick={handleLogout}>Logout</button>
+        </Header>
         <Content style={{ margin: "24px 16px 0" }}>
           <div
             style={{
