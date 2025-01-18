268ee019ad60f3073b999a4ad8eba160893f5f6d
Rakesh Biswas <rbiswas01999@gmail.com>
2025-01-18 10:03:23 +0600

    baseApi build

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,11 +1,19 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const baseApi = createApi({
-  reducerPath: 'baseApi',
-  baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:5000/api/v1',
-    credentials: 'include',
-  }),
+    reducerPath: 'baseApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'http://localhost:5000/api/v1',
+        credentials: 'include',
+    }),
 
-  endpoints: () => ({}),
+    endpoints: (builder) => ({
+        login: builder.mutation({
+            query: (userInfo) => ({
+                url: '/auth/login',
+                method: 'POST',
+                body: userInfo,
+            }),
+        }),
+    }),
 });
