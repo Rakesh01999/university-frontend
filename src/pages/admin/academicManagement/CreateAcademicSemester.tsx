46bc3ed7bdbf217622e0a75d7f455a91e28b1085
Rakesh Biswas <rbiswas01999@gmail.com>
2025-01-22 19:16:54 +0600

    useAddAcademicSemesterMutation imported correctly

--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -7,9 +7,9 @@ import { monthOptions } from '../../../constants/global';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 import { academicSemesterSchema } from '../../../schemas/academicManagement.schema';
-import { useAddAcademicSemesterMutation } from '../../../redux/features/admin/academicManagement.api';
 import { toast } from 'sonner';
 import { TResponse } from '../../../types/global';
+import { useAddAcademicSemesterMutation } from '../../../redux/features/admin/academicManagement.api';
 
 const currentYear = new Date().getFullYear();
 const yearOptions = [0, 1, 2, 3, 4].map((number) => ({
