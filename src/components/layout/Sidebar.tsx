4cf2c6fdfa0bd783ed49b8fdbae7e1faf38e87c9
Rakesh Biswas <rbiswas01999@gmail.com>
2025-01-22 19:46:48 +0600

    Layout, Sidebar fixed

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,33 +1,3 @@
-// import { Layout, Menu } from "antd";
-// import { sidebarItemsGenerator } from "../../utils/sidebarItemsGenerator";
-// import { adminPaths } from "../../routes/admin.routes";
-// const { Sider } = Layout;
-// const Sidebar = () => {
-//   return (
-//     <Sider breakpoint="lg" collapsedWidth="0">
-//       <div
-//         style={{
-//           color: "white",
-
-//           height: "4rem",
-//           display: "flex",
-//           justifyContent: "center",
-//           alignItems: "center",
-//         }}
-//       >
-//         <h1>PH University</h1>
-//       </div>
-//       <Menu
-//         theme="dark"
-//         mode="inline"
-//         defaultSelectedKeys={["4"]}
-//         items={sidebarItemsGenerator(adminPaths, "admin")}
-//       />
-//     </Sider>
-//   );
-// };
-// export default Sidebar;
-
 import { Layout, Menu } from 'antd';
 import { sidebarItemsGenerator } from '../../utils/sidebarItemsGenerator';
 import { adminPaths } from '../../routes/admin.routes';
@@ -45,9 +15,6 @@ const userRole = {
 };
 
 const Sidebar = () => {
-//   const role = 'admin';
-//   const role = 'faculty';
-  // const role = 'student';
   const user = useAppSelector(selectCurrentUser);
 
   let sidebarItems;
@@ -68,7 +35,11 @@ const Sidebar = () => {
   }
 
   return (
-    <Sider breakpoint="lg" collapsedWidth="0">
+    <Sider
+      breakpoint="lg"
+      collapsedWidth="0"
+      style={{ height: '100vh', position: 'sticky', top: '0', left: '0' }}
+    >
       <div
         style={{
           color: 'white',
